d4a92ede4332942ea677ca625f7fe0cb56204962
KongariBug <kongaribug@gmail.com>
2021-08-02 09:47:17 +0900

    Init the profile card.

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,16 +1,46 @@
 import React from 'react'
 import Image from "next/image"
 import styles from "../styles/Profile.module.css"
-
-const myLoader = ({ src, width, quality }) => {
-  return `${src}?w=${width}&q=${quality || 75}`
-}
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 export const Profile: React.FC = () => (
   <div className={styles.container}>
-    <Image loader={myLoader} src={'/images/okoyan_kill_aqua.png'} width={100} height={100} />
-    <h1 className={styles.title}>
-      たづ
-    </h1>
+
+    <div className={styles.image}>
+      <Image src={'/images/okoyan_kill_aqua.png'} width={510} height={509}/>
+    </div>
+
+    <div className={styles.description}>
+      <div>
+        フリーランス エンジニア
+      </div>
+      <div className={styles.name}>
+        たづ
+      </div>
+      <div className={styles['sns-contents']}>
+        <FontAwesomeIcon icon={faTwitter} className={`${styles['twitter-icon']} ${styles['sns-icon']}`} />
+        <span className={styles['sns-username']}>
+          <a href={'https://twitter.com/Kongari_Bug'}>
+            @Kongari_Bug
+          </a>
+        </span>
+      </div>
+      <div className={styles['sns-contents']}>
+        <FontAwesomeIcon icon={faGithub} className={styles['sns-icon']} />
+        <span className={styles['sns-username']}>
+          <a href={'https://github.com/tazu-yyy'}>
+            tazu-yyy
+          </a>
+        </span>
+      </div>
+      <div className={styles['sns-contents']}>
+        <FontAwesomeIcon icon={faEnvelope} className={styles['sns-icon']} />
+        <span className={styles['sns-username']}>
+          kongaribug★gmail.com
+        </span>
+      </div>
+    </div>
   </div>
 )
