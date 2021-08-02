26aab74a64ea4fdb3c067b0a0e9941bf06a46d36
tazu-yyy <tazu-yyy@users.noreply.github.com>
2021-08-02 01:40:02 +0000

    deploy: d4a92ede4332942ea677ca625f7fe0cb56204962

--- _next/static/bwfeucbUrkc9hXVn4iG-H/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/57ead2c66ddf9fd723a3.css","static/chunks/pages/index-b9b2db256c00fd412a3c.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
