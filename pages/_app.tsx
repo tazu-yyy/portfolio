a4dd32a9f5f53275d6dada88524b9724154cb1ea
KongariBug <kongaribug@gmail.com>
2021-09-07 18:09:28 +0900

    Fix transition bugs.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import '../styles/globals.scss'
 import { AnimatePresence } from "framer-motion";
+import { useTransitionFix } from "../lib/useTransitionFix";
 
 function MyApp({ Component, pageProps, router }) {
+  const transitionCallback = useTransitionFix()
+
   return (
     <div>
-      <AnimatePresence exitBeforeEnter>
+      <AnimatePresence exitBeforeEnter onExitComplete={transitionCallback}>
         <Component {...pageProps} key={router.route} />
       </AnimatePresence>
     </div>
