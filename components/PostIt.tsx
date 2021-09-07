b1847067f6b4672cfc2df7b09bcbe4b7c0f13b98
KongariBug <kongaribug@gmail.com>
2021-09-07 17:55:06 +0900

    Fix type error.

--- components/PostIt.tsx
+++ components/PostIt.tsx
@@ -4,7 +4,12 @@ import styles from "../styles/PostIt.module.scss"
 // import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
 // import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
-export const PostIt: React.FC = (props) => {
+interface PostItProps {
+  title: string,
+  url?: string
+}
+
+export const PostIt: React.FC<PostItProps> = (props) => {
   return (
     <div className={styles["fusen-container"]}>
       <div className={styles.fusen}>
