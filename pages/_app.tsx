1590c461e466b796eb9149d3ed687892c8ae1804
KongariBug <kongaribug@gmail.com>
2021-09-29 14:12:09 +0900

    Remove motion animations.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,10 @@
 import '../styles/globals.scss'
 import { AnimatePresence } from "framer-motion";
-import { useTransitionFix } from "../lib/useTransitionFix";
 
 function MyApp({ Component, pageProps, router }) {
-  const transitionCallback = useTransitionFix()
-
   return (
     <div>
-      <AnimatePresence onExitComplete={transitionCallback}>
+      <AnimatePresence>
         <Component {...pageProps} key={router.route} />
       </AnimatePresence>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,43 +2,36 @@ import styles from '../styles/Home.module.scss'
 import { Profile } from '../components/Profile'
 import Link from 'next/link';
 import { CommonHead } from "../components/CommonHead";
-import { motion } from "framer-motion"
 
 export default function Home() {
   return (
       <div className={styles.container}>
-        <motion.span
-          initial={{ x: "-200%" }}
-          animate={{ x: "0" }}
-          transition={{ ease: "easeOut", duration: 0.25 }}
-        >
-          <CommonHead />
-          <main className={styles.main}>
-            <div className={`${styles["hobby-container"]} ${styles["menu-container"]}`}>
-              <Link href={"/hobby"}>
-                <a>
-                  趣味
-                  <img src={"/images/controller2.png"} className={`${styles["hobby-image"]} ${styles["menu-image"]}`}/>
-                </a>
-              </Link>
-            </div>
-            <div className={`${styles["sp-container"]} ${styles["menu-container"]}`}>
-              <Link href={"/products"}>
-                <a>
-                  制作物
-                  <img src={"/images/sp.png"} className={`${styles["sp-image"]} ${styles["menu-image"]}`} />
-                </a>
-              </Link>
-            </div>
-            <div className={`${styles["book-container"]} ${styles["menu-container"]}`}>
-              <a href={"https://kongaribug.hatenablog.com/"}>
-                ブログ
-                <img src={"/images/book.png"} className={`${styles["book-image"]} ${styles["menu-image"]}`} />
+        <CommonHead />
+        <main className={styles.main}>
+          <div className={`${styles["hobby-container"]} ${styles["menu-container"]}`}>
+            <Link href={"/hobby"}>
+              <a>
+                趣味
+                <img src={"/images/controller2.png"} className={`${styles["hobby-image"]} ${styles["menu-image"]}`}/>
               </a>
-            </div>
-            <Profile />
-          </main>
-        </motion.span>
+            </Link>
+          </div>
+          <div className={`${styles["sp-container"]} ${styles["menu-container"]}`}>
+            <Link href={"/products"}>
+              <a>
+                制作物
+                <img src={"/images/sp.png"} className={`${styles["sp-image"]} ${styles["menu-image"]}`} />
+              </a>
+            </Link>
+          </div>
+          <div className={`${styles["book-container"]} ${styles["menu-container"]}`}>
+            <a href={"https://kongaribug.hatenablog.com/"}>
+              ブログ
+              <img src={"/images/book.png"} className={`${styles["book-image"]} ${styles["menu-image"]}`} />
+            </a>
+          </div>
+          <Profile />
+        </main>
       </div>
   )
 }
